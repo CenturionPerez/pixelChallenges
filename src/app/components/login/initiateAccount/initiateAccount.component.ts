3746acf58fbec5e15683b8a2be7ccfcd94aa5fd6
centurionperez <centurionperez98@gmail.com>
2023-11-29 21:10:45 +0100

    feat: [APB] new changes in PixelChallenges app

---
+++ src/app/components/login/initiateAccount/initiateAccount.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'initiateAccount',
+  templateUrl: './initiateAccount.component.html',
+  styleUrls: ['./initiateAccount.component.css']
+})
+export class InitiateAccountComponent { }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule { }
