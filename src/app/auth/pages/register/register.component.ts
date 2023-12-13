79b1528fe7edd2cf717fce35779718549a9622d6
Alberto <bertomieres86@gmail.com>
2023-12-13 21:00:01 +0100

    FIX: Modules fixed

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent { }
+export class RegisterComponent {
+  constructor(private router: Router){}
+
+    public navigateBack(){
+      this.router.navigateByUrl('/init')
+    }
+
+}
