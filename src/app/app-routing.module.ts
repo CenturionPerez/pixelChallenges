b6bdc2cc43042c85a4bd76ffe53b4848e2eaa9d9
centurionperez <centurionperez98@gmail.com>
2023-12-13 20:13:03 +0100

    feat: [APB] new changes in PixelChallenges about login component css and hmtl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
-    { path: 'login', 
+    { path: '', 
       loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
     },
     { path: '404',
       loadChildren: () => import('./error/error.module').then(m => m.ErrorModule),
     },
     { path: '**', redirectTo: '404'}
-
   ];
 
 @NgModule({
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
                 component: RegisterComponent,
             },
             {
+              path: '',
+              component: InitComponent,
+            },
+            {
                 path: 'init',
                 component: InitComponent,
             }
