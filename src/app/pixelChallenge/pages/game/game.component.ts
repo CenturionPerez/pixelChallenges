8b1f55fdc7bc3fc4bad4660d5854af5b98e3b04b
Alberto <bertomieres86@gmail.com>
2024-01-11 06:06:49 +0100

    changes in nav bar, routing to game(mainteniance page), invited mode in login

--- src/app/pixelChallenge/pages/game/game.component.ts
+++ src/app/pixelChallenge/pages/game/game.component.ts
@@ -1,9 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent { }
+export class GameComponent {
+
+  constructor(
+    private router: Router
+  ){}
+
+
+
+}
---
+++ src/app/pixelChallenge/pages/profile/profile.component.ts
@@ -0,0 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.componen.html'
+})
+export class ProfileComponent { }
