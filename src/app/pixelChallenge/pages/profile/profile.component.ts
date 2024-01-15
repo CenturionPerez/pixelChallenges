a2d38d634eed3f9b94f1342a94098e11f5085f6c
Alberto <bertomieres86@gmail.com>
2024-01-15 18:40:38 +0100

    profile ts/html/css

--- src/app/pixelChallenge/pages/profile/profile.component.ts
+++ src/app/pixelChallenge/pages/profile/profile.component.ts
@@ -1,13 +1,7 @@
-import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, Validators  } from '@angular/forms';
-import { RegisterComponent } from 'src/app/auth/pages/register/register.component';
-import { literals } from 'src/app/utils/interfaces/util.constants';
-import { Literals } from 'src/app/utils/interfaces/util.interface';
-import { AuthService } from '../../../auth/services/auth.service';
+import { Component} from '@angular/core';
 import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { UpdateClient } from 'src/app/auth/interfaces/auth.interface';
-import { ClientRegister } from 'src/app/auth/interfaces/auth.interface';
+
+
 
 @Component({
   selector: 'profile',
@@ -16,106 +10,31 @@ import { ClientRegister } from 'src/app/auth/interfaces/auth.interface';
 })
 export class ProfileComponent {
 
- public datosRegistro: ClientRegister | null = null;
- public literals: Literals = literals;
- public updateProfileForm: FormGroup = new FormGroup({
-  username: new FormControl('',[
-    Validators.minLength(3)
-  ]),
-  email: new FormControl('', [
-    Validators.email
-  ]),
-  password: new FormControl('', [
-
-  ]),
-  msisdn: new FormControl('', [
-    Validators.minLength(9),
-    Validators.maxLength(9)
-  ])
- });
-
- ngOnInit(): void {
-
-    const usuariosRegistro = localStorage.getItem('usuariosRegistro');
-
-    if(usuariosRegistro){
-
-      this.datosRegistro = JSON.parse(usuariosRegistro);
-
-      console.log(this.datosRegistro);
-      this.updateProfileForm.setValue({
-        username: this.datosRegistro!.name,
-        email: this.datosRegistro!.email,
-        msisdn: this.datosRegistro!.msisdn
-      })
-    } else {
-        console.error ('No se encontraron datos de registro en localStorage.');
-      }
-  }
-
-  borrarDatos():void {
-    localStorage.removeItem('datosRegistro');
-
-    this.datosRegistro = null;
-
-  }
+  constructor(private router: Router) { }
 
 
- constructor(
+  showInputs: boolean = false;
+  showImg: boolean = false;
 
-  private authService: AuthService,
-  private _snackbar: MatSnackBar,
-  private router: Router  ){}
+ public changeImage(): void {
 
-  public prepareRequest(): UpdateClient {
-    return {
-      name: this.updateProfileForm.get('username')?.value,
-      email: this.updateProfileForm.get('email')?.value,
-      password: this.updateProfileForm.get('password')?.value,
-      msisdn: this.updateProfileForm.get('msisdn')?.value,
-    }
+ }
 
-  }
+ public showInputPass(): void {
 
+  this.showInputs = !this.showInputs;
 
+ }
+
+ public showInputImg(): void {
+  this.showImg = !this.showImg;
+ }
+
+ public confirmarCambio(): void {
+
+ }
 
   public update(): void {
-    if(this.updateProfileForm.valid){
-      this.authService.showAuthSpinner(true);
 
-      // Obtener datos del formulario
-
-      const formData = this.updateProfileForm.value;
-
-      const updatedProfile = {
-        name: formData.username,
-        email: formData.email,
-        msisdn: formData.msisdn,
-      }
-
-      // Actualizar localStorage con los nuevos datos del perfil
-
-      localStorage.setItem('datosRegistro', JSON.stringify(updatedProfile));
-
-      this.authService.updateUser(this.prepareRequest()).subscribe((resp) => {
-        this.authService.showAuthSpinner(false);
-        if(resp){
-          this.router.navigateByUrl('profile');
-          this.authService.setUserLoggedInSessionStorage(true);
-          this.authService.generateSnackBar(false, literals.success_update);
-          this.updateProfileForm.setValue({
-            username: updatedProfile.name,
-            email: updatedProfile.email,
-            msisdn: updatedProfile.msisdn,
-          })
-        }
-        else{
-          this.authService.setUserLoggedInSessionStorage(true);
-          this.authService.generateSnackBar(true, literals.error_update);
-        }
-      });
-    }else{
-      this.authService.generateSnackBar(true, literals.error_form)
-    }
   }
 }
